39d74e08a134f77ef782eff074b68ae93f6591e5
lillimao <lisamensa@gmail.com>
2017-12-28 12:12:11 +0100

    inizio

---
+++ EsCarrello/src/BigliettoAuguri.java
@@ -0,0 +1,4 @@
+
+public class BigliettoAuguri extends Prodotto{
+
+}
---
+++ EsCarrello/src/Carrello.java
@@ -0,0 +1,17 @@
+
+public class Carrello {
+	
+	int tot;
+	
+	public Carrello(int tot) {
+		this.tot = tot;		
+	}
+	
+	public boolean aggiungiProdotto(Prodotto prod) {
+		boolean aggiunto = true;
+		return true;
+	}
+	
+	
+
+}
